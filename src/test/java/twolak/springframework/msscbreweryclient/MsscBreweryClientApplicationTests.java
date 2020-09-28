78f303e8cbecefad9e1ad7ed8f0895f356c24079
twolak88 <twolak88@gmail.com>
2020-09-28 19:26:54 +0200

    Create initial project. closes #1

---
+++ src/test/java/twolak/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package twolak.springframework.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
