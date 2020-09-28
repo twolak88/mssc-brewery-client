931b2d69bda2a75d0cb6f6e20f6d4e235695ae44
twolak88 <twolak88@gmail.com>
2020-09-28 20:48:17 +0200

    REST template for GET. closes #4

--- src/main/java/twolak/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/twolak/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,7 +1,11 @@
 package twolak.springframework.msscbreweryclient.web.client;
 
+import java.util.UUID;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import twolak.springframework.msscbreweryclient.web.model.BeerDto;
 
 /**
  *
@@ -11,8 +15,18 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(value = "tw.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
     
-    public static final String BEER_PATH_V1 = "/api/v1/beer";
+    public static final String BEER_PATH_V1 = "/api/v1/beer/";
     private String apiHost;
+    
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+    
+    public BeerDto getBeerById(UUID beerId) {
+        return this.restTemplate.getForObject(this.apiHost + BEER_PATH_V1 + beerId, BeerDto.class);
+    }
     
     public void setApiHost(String apiHost) {
         this.apiHost = apiHost;
