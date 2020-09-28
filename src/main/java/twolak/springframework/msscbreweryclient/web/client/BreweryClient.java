7b07fb7bfda22ac7ab78542c5939d8b980b70bee
twolak88 <twolak88@gmail.com>
2020-09-28 20:21:21 +0200

    Host name property. closes #3

---
+++ src/main/java/twolak/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,21 @@
+package twolak.springframework.msscbreweryclient.web.client;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author twolak
+ */
+@Component
+@ConfigurationProperties(value = "tw.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+    
+    public static final String BEER_PATH_V1 = "/api/v1/beer";
+    private String apiHost;
+    
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
