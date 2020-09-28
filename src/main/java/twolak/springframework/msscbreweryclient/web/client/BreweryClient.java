adff9613e7e0531ee8ca3f29a661ee86492cb308
twolak88 <twolak88@gmail.com>
2020-09-28 20:22:48 +0200

    Clean import

--- src/main/java/twolak/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/twolak/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,6 +1,5 @@
 package twolak.springframework.msscbreweryclient.web.client;
 
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
